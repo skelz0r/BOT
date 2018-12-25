73ca34bd2346ef144c0a54e4c2ac36fa623150a6
Loïc Delmaire <loic.delmaire@gmail.com>
2018-12-25 18:47:24 +0100

    Introduce RSpec

--- Gemfile
+++ Gemfile
@@ -4,3 +4,7 @@ gem "cinch"
 gem "text-hyphen"
 gem "ruby-sox"
 gem "espeak-ruby", require: "espeak"
+
+group :development, :test do
+  gem "rspec"
+end
---
+++ spec/lib/sentence_to_sound_sample_spec.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+require 'sentence_to_sound_sample'
+
+describe SentenceToSoundSample do
+  let(:service) { SentenceToSoundSample.new(sentence) }
+  let(:sentence) { "LOL" }
+
+  before do
+    allow_any_instance_of(SentenceToSoundSample).to receive(:sound_options).and_return({
+      pitch: 100,
+      speed: 1.0,
+    })
+
+    ENV['SOUNDS_PATH'] = File.expand_path(File.join(File.dirname(__FILE__), '../../sounds/')) + '/'
+  end
+
+  describe "#perform" do
+    subject { service.perform }
+
+    it "creates a file" do
+      expect {
+        subject
+      }.to change{Dir.new(ENV['SOUNDS_PATH']).entries.count}.by(1)
+    end
+  end
+end
