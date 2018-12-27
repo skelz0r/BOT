f92d4396cf5c22ab99f1afeedee6a8a2e2736693
Loïc Delmaire <loic.delmaire@gmail.com>
2018-12-27 12:30:37 +0100

    Move Autisme

--- bot.rb
+++ bot.rb
@@ -3,8 +3,16 @@ $:.unshift File.dirname(__FILE__)
 require 'cinch'
 require 'uri'
 
-require 'lib/syllabe_extractor'
-require 'lib/sentence_to_sound_sample'
+[
+  'lib/',
+  'lib/interceptor/',
+].each do |path|
+  Dir.entries(path).each do |file|
+    next unless file =~ /\.rb$/
+
+    require "#{path}#{file.gsub(/\.rb$/, '')}"
+  end
+end
 
 if ENV["DEBUG"]
   NICK = "BOTDEBUG"
@@ -26,38 +34,6 @@ else
   ]
 
   RAND = 30
-end
-
-class Austisme
-  def initialize(event, last_autisme, last_victim)
-    @tence = SyllabeExtractor.new(event.message)
-    @sentence = @tence.perform
-
-    @event = event
-    @last_autisme = last_autisme
-    @last_victim = last_victim
-  end
-
-  def mad?
-    (rand(RAND) == 0 || ( @last_autisme && @sentence == @last_autisme.say && @last_victime != @event.user.nick))
-  end
-
-  def madness!
-    @speeque = SentenceToSoundSample.new(@sentence)
-    @speeque.perform
-  end
-
-  def say
-    @sentence
-  end
-
-  def sound_url
-    @speeque.url
-  end
-
-  def sound_path
-    @speeque.path
-  end
 end
 
 bot = Cinch::Bot.new do
---
+++ lib/autisme.rb
@@ -0,0 +1,27 @@
+class Austisme
+  def initialize(event, last_autisme, last_victim)
+    @tence = SyllabeExtractor.new(event.message)
+    @sentence = @tence.perform
+
+    @event = event
+    @last_autisme = last_autisme
+    @last_victim = last_victim
+  end
+
+  def mad?
+    (rand(RAND) == 0 || ( @last_autisme && @sentence == @last_autisme.say && @last_victime != @event.user.nick))
+  end
+
+  def madness!
+    @speeque = SentenceToSoundSample.new(@sentence)
+    @speeque.perform
+  end
+
+  def say
+    @sentence
+  end
+
+  def sound_url
+    @speeque.url
+  end
+end
