16c2bc03525a86ac2a03190d34aaa8b37f84d478
Martin Keogh <martin1keogh@gmail.com>
2019-01-10 10:58:14 +0100

    Refactor EohInterceptor a bit (move answer list into a def of its own)

--- lib/interceptor/eoh_interceptor.rb
+++ lib/interceptor/eoh_interceptor.rb
@@ -4,32 +4,6 @@ class EohInterceptor < BaseInterceptor
   end
 
   def reply
-    answer =
-      if rand(amora_rand) == 1
-        'TG Amora'
-      else
-        [
-          'On se clap au fond svp',
-          'On se corn au fond svp',
-          'On se clame au fond svp',
-          'On se klam au fond svp',
-          'On se clape au fond svp',
-          'On se skelz0r au fond svp',
-          'On se thurolf au fond svp',
-          'Ça coinche au fond svp',
-          'C\'est coinché au fond svp',
-          'On se stuff au fond svp',
-          'On se ghost au fond svp',
-          'On se profile au fond svp',
-          'On se pakito au fond svp',
-          'On se guinness dans le hall svp',
-          'On picole au fond svp',
-          'On se canne au fond svp',
-          'Ça compact au fond svp',
-          'On s\'égare au fond svp',
-        ].sample
-      end
-
     if uppercase_message?
       answer.upcase
     else
@@ -38,6 +12,37 @@ class EohInterceptor < BaseInterceptor
   end
 
   private
+
+  def answer
+    if rand(amora_rand) == 1
+      'TG Amora'
+    else
+      answers.sample
+    end
+  end
+
+  def answers
+    [
+      'On se clap au fond svp',
+      'On se corn au fond svp',
+      'On se clame au fond svp',
+      'On se klam au fond svp',
+      'On se clape au fond svp',
+      'On se skelz0r au fond svp',
+      'On se thurolf au fond svp',
+      'Ça coinche au fond svp',
+      'C\'est coinché au fond svp',
+      'On se stuff au fond svp',
+      'On se ghost au fond svp',
+      'On se profile au fond svp',
+      'On se pakito au fond svp',
+      'On se guinness dans le hall svp',
+      'On picole au fond svp',
+      'On se canne au fond svp',
+      'Ça compact au fond svp',
+      'On s\'égare au fond svp',
+    ]
+  end
 
   def amora_rand
     50
